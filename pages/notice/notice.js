3516016aaf9346e670d6a1b4b6024ac89dce3c59
董立 <caocong1@gmail.com>
2017-09-20 20:01:42 +0800

    2

--- pages/notice/notice.js
+++ pages/notice/notice.js
@@ -4,7 +4,14 @@ var appgl = getApp().globalData;
 Page({
   data: {
     title:"通知公告",
-    titleicon:"fa-envelope-open"
+    titleicon:"fa-envelope-open",
+    item: {
+      isread: false,
+      noticetitle: "9月11日全体店长会",
+      noticecontent: "9月11日在B1会议室召开全体...",
+      noticetime: "2017-9-10 12:24:33",
+      hasattr: true
+    }
   },
   onReady: function () {
     appgl.open = this.data.open
