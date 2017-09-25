b6276ebc4b2f2f89db0182661291021eb102436c
caocong1 <caocong1@users.noreply.github.com>
2017-09-26 02:00:14 +0800

    0926

--- pages/vericancel/vericancel.js
+++ pages/vericancel/vericancel.js
@@ -2,31 +2,41 @@
 var s = require('../../shopclient.js');
 var appgl = getApp().globalData;
 var scancode;
+
 Page({
   data: {
     title: "卡券核销",
-    open: false
+    titleicon: "fa-thumbs-up",
+    open: false,
+    opencoupon:false,
+    confirmbtname: '查询卡券',
+    coupondisplay:'none'
   },
-  inputscancode: function (e) {
-    console.log(e)
+  inputscancode(e) {
     scancode = e.detail.value
   },
-  onReady: function () {
+  onReady() {
     appgl.open = this.data.open
+    const animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear',
+      // transformOrigin:'100% 0'
+    })
+    this.animation=animation
   },
-  tap_ch: function () {
+  tap_ch() {
     s.toggle(this)
   },
-  tap_start: function (e) {
+  tap_start(e) {
     s.dragstart(e)
   },
-  tap_drag: function (e) {
+  tap_drag(e) {
     s.drag(e)
   },
-  tap_end: function (e) {
+  tap_end(e) {
     s.dragend(this)
   },
-  scancode: function(){
+  scancode(){
     wx.scanCode({
       success: (res) => {
         console.log(res)
@@ -35,6 +45,30 @@ Page({
         })
       }
     })
+  },
+  confirm(){
+    if(this.opencoupon){
+      this.opencoupon=false
+      this.setData({
+        coupondisplay: 'none',
+        confirmbtname:'查询卡券'
+      })
+      // this.animation.scaleY(0).step()
+      // this.setData({
+      //   anicoupon: this.animation.export()
+      // })
+    }else{
+      this.opencoupon = true
+      this.animation.opacity(0).step()
+      this.setData({
+        coupondisplay:'block',
+        anicoupon: this.animation.export(),
+        confirmbtname: '确定使用'
+      })
+      // this.animation.height('100%').step()
+      // this.setData({
+      //   anicoupon: this.animation.export()
+      // })
+    }
   }
-
 })
